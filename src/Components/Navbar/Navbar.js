303748dd557c82542a3155930250037784038e2b
Aco Vidovič <acovid@gmail.com>
2024-12-27 21:05:02 +0100

    enhance navbar to show conditional menu options dependent on whether a user is logged in

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,65 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./Navbar.css"
-import logo from './stay-healthy-logo.png'
-import login from './login.svg'
+import logo from "./stay-healthy-logo.png"
+import login from "./login.svg"
 
 // Define a functional component
 const Navbar = () => {
+  // BLOCK: determine if a user is logged in
+  const [name, setName] = useState("")
+  const [signUpOrWelcome, setSignUpOrWelcome] = useState("")
+  const [loginOrLogout, setLoginOrLogout] = useState("")
+  // sessionStorage.setItem("name", "Aco")
+
+  // use later the const name to check fpr user's name in sessions storage
+  useEffect(() => {
+    // When the component mounts, check if there's a 'name' value in Session Storage.
+    const storedName = sessionStorage.getItem("name")
+
+    // this if statement will determine what to show in teh menu: Sign Up & Login or Welcome, user and Logout
+    if (sessionStorage.getItem("name") !== "") {
+      // user is logged in
+      setName(storedName)
+      console.log("-- Yes, there is a name in the session storage! This one:", storedName)
+      setSignUpOrWelcome(<li className="welcome_user">Welcome {storedName}</li>)
+      setLoginOrLogout(
+        <li className="link" onClick={()=>{
+          sessionStorage.setItem("name", "")
+          alert('I logged out')
+        }}>
+          <a href="/">
+            {/* <img className="login_icon" src={login} alt="Enter" /> */}
+            <button className="btn1">Logout</button>
+          </a>
+        </li>
+      )
+      console.log("signUpOrWelcome:", signUpOrWelcome);
+      console.log("loginOrLogout:", loginOrLogout);      
+      // sessionStorage.setItem("name", null)
+    } else {
+      // no user logged in
+      console.log("-- No name in the session storage.")
+      setSignUpOrWelcome(
+        <li className="link">
+          <a href="/sign-up">
+            <button className="btn1">Sign Up</button>
+          </a>
+        </li>
+      )
+      setLoginOrLogout(
+        <li className="link">
+          <a href="/Login">
+            {/* <img className="login_icon" src={login} alt="Enter" /> */}
+            <button className="btn1">Login</button>
+          </a>
+        </li>
+      )
+      console.log("signUpOrWelcome:", { signUpOrWelcome })
+      // Save the entered name to Session Storage.
+    }
+  }, [])
+  // END BLOCK: determine if the user is logged in
+
   return (
     <nav>
       {/* Navigation logo section */}
@@ -30,21 +85,25 @@ const Navbar = () => {
         <li className="link">
           <a href="#">Appointments</a>
         </li>
+
+        {/* show either Sign Up or Welcome - depending f the user is logged in */}
+        {signUpOrWelcome}
         {/*  List item for the 'Sign Up' link with a button  */}
-        <li className="link">
-        {/* <a href="../Sign_Up/Sign_Up.html"> */}
-        <a href="/sign-up">
+        {/* <li className="link">
+          <a href="/sign-up">
             <button className="btn1">Sign Up</button>
           </a>
-        </li>
+        </li> */}
 
+        {/* show either Login or Logout - depending f the user is logged in */}
+        {loginOrLogout}
         {/*  List item for the 'Login' link with a button  */}
-        <li className="link">
+        {/* <li className="link">
           <a href="/Login">
             {/* <img className="login_icon" src={login} alt="Enter" /> */}
-            <button className="btn1">Login</button>
+        {/* <button className="btn1">Login</button>
           </a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   )
